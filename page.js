17c1ed060c12c5f8543f6bdc1be1d12e7683ffe2
SHAH HUZAIFA <143290124+shahhuzaifa@users.noreply.github.com>
2023-10-06 14:23:34 +0500

    Add files via upload

---
+++ page.js
@@ -0,0 +1,37 @@
+// var hi = new Date();
+// document.write(hi);
+
+
+// var firsName = prompt("enter your first name ");
+// var lastName = prompt("enter your last name ");
+// var fullName = firsName + " " + lastName;
+// alert(fullName +  " " + "welcome to website");
+
+// var num1 = +prompt("ENTER VALUE FOR SUM ");
+// var num2 = +prompt("ENTER SECOND VALUE FOR SUM " );
+
+// var SUM  = num1 + num2 ;
+// alert(SUM);
+
+
+// function square(number) {
+//     return number * number;
+//   }
+  
+  
+//   var originalNumber = 5;
+//   var squaredNumber = square(originalNumber);
+  
+//   console.log(squaredNumber);
+  
+
+// function counting(start , end ){
+//   if (start <= end){
+//     for(var i = start; i<=end; i++){
+//       document.write(i + "<br>")
+//     }
+//   }
+// }
+// var start = 1 ;
+// var end  = 10 ; 
+// counting(start,end)
